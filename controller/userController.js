d7bccd30032c430bd03f5dff79ee453982236a95
robiulalam76 <robiulalam762400@gmail.com>
2023-04-20 11:43:01 +0600

    register validation added

--- controller/userController.js
+++ controller/userController.js
@@ -39,6 +39,8 @@ const verifyEmailAddress = async (req, res) => {
   }
 };
 
+
+// register user
 const registerUser = async (req, res) => {
   const {
     name,
@@ -49,38 +51,54 @@ const registerUser = async (req, res) => {
     phone,
     role,
   } = req.body;
-  const isAdded = await User.findOne({ email: email });
 
-  console.log(req.body, isAdded);
+  const findCompany = await User.findOne({ company: company });
+  console.log(req.body);
 
-  if (isAdded) {
-    const token = signInToken(isAdded);
-    return res.send({
-      token,
-      name: isAdded.name,
-      email: isAdded.email,
-      message: "Email Already Verified!",
-    });
-  }
+  try {
+    if (findCompany) {
+      return res.status(200).json({
+        status: "error",
+        message: { companyMessage: "Company Name Already in Use" },
+        data: req.body.company
+      });
+    }
+    else {
+      const isAdded = await User.findOne({ email: email });
+      if (isAdded) {
+        return res.status(200).json({
+          status: "error",
+          message: { emailMessage: "Email Already in Use" },
+          data: req.body.email
+        });
+      }
+      else {
+        const newUser = new User({
+          name,
+          email,
+          company,
+          country,
+          phone,
+          role,
+          password: bcrypt.hashSync(password),
+        });
+        newUser.save();
+        // const token = signInToken({ name, email, password: bcrypt.hashSync(password) });
+        res.send({
+          success: true,
+          _id: newUser._id,
+          name: newUser.name,
+          email: newUser.email,
+          message: "Please Login Now!",
+        });
+      }
+    }
 
-  else {
-    const newUser = new User({
-      name,
-      email,
-      company,
-      country,
-      phone,
-      role,
-      password: bcrypt.hashSync(password),
-    });
-    newUser.save();
-    // const token = signInToken({ name, email, password: bcrypt.hashSync(password) });
-    res.send({
-      success: true,
-      _id: newUser._id,
-      name: newUser.name,
-      email: newUser.email,
-      message: "Email Verified, Please Login Now!",
+  } catch (error) {
+    res.status(400).json({
+      status: "error",
+      message: "Data couldn't insert z",
+      error: error.message,
     });
   }
 };
--- models/User.js
+++ models/User.js
@@ -20,7 +20,7 @@ const userSchema = new mongoose.Schema(
     },
     company: {
       type: String,
-      required: false,
+      required: true,
     },
     city: {
       type: String,
