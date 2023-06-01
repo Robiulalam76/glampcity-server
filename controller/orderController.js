c6a858ad171f9f12ec094462c736fcf0090833fb
Robiulalam76 <robiulalam762400@gmail.com>
2023-06-01 20:17:54 +0600

    get orders by userid with pagenation

--- controller/orderController.js
+++ controller/orderController.js
@@ -1,3 +1,4 @@
+const CartProduct = require("../models/CartProductModel");
 const Order = require("../models/OrderModel");
 
 const createOrder = async (req, res) => {
@@ -14,10 +15,108 @@ const createOrder = async (req, res) => {
             customer: req.body.customer,
             products: req.body.products
         })
-        await newOrder.save()
+        const result = await newOrder.save()
+        if (result) {
+            const removeProducts = await CartProduct.deleteMany({
+                $and: [
+                    { user: req.body.customer },
+                    { store: req.body.store },
+                ]
+            });
+            if (removeProducts) {
+                res.status(200).json({
+                    status: "success",
+                    message: "New Order is Success"
+                });
+            }
+
+        }
+    } catch (error) {
+        res.status(400).json({
+            status: "error",
+            message: "Data couldn't be retrieved",
+            error: error.message,
+        });
+    }
+}
+
+
+// get orders by userId
+// const getOrdersByUserId = async (req, res) => {
+//     try {
+//         const orders = await Order.find({ customer: req.params.userId }).sort({ _id: -1 })
+//         res.status(200).send(orders);
+//     } catch (error) {
+//         res.status(400).json({
+//             status: "error",
+//             message: "Data couldn't be retrieved",
+//             error: error.message,
+//         });
+//     }
+// }
+
+const getOrdersByUserId = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+        const page = parseInt(req.query.page) || 1; // Get the page number from the query parameters (default: 1)
+        const limit = 10; // Number of orders per page
+        const startIndex = (page - 1) * limit; // Calculate the starting index for the page
+
+        const totalOrders = await Order.countDocuments({ customer: userId }); // Get the total number of orders for the user
+        const totalPages = Math.ceil(totalOrders / limit); // Calculate the total number of pages
+
+        const orders = await Order.find({ customer: userId })
+            .sort({ _id: -1 })
+            .skip(startIndex)
+            .limit(limit);
+
+        res.status(200).json({
+            orders,
+            page,
+            totalPages,
+            totalOrders,
+        });
+    } catch (error) {
+        res.status(400).json({
+            status: "error",
+            message: "Data couldn't be retrieved",
+            error: error.message,
+        });
+    }
+};
+
+
+
+// get orders by userId
+const getOrdersByStoreId = async (req, res) => {
+    try {
+        const orders = await Order.find({ store: req.params.storeId })
+        res.status(200).send(orders);
+    } catch (error) {
+        res.status(400).json({
+            status: "error",
+            message: "Data couldn't be retrieved",
+            error: error.message,
+        });
+    }
+}
+
+// get orders by userId
+const updateShippingStatusByStoreAndOrderId = async (req, res) => {
+    try {
+        const result = await Order.findOneAndUpdate({
+            $and: [
+                { store: req.params.storeId },
+                { _id: req.params.orderId }
+            ]
+        },
+            { $set: { shippingStatus: req.body.shippingStatus } },
+            { new: true }
+        );
         res.status(200).json({
             status: "success",
-            message: "New Order is Success"
+            message: "status update success",
+            result: result
         });
     } catch (error) {
         res.status(400).json({
@@ -31,4 +130,7 @@ const createOrder = async (req, res) => {
 
 module.exports = {
     createOrder,
+    getOrdersByUserId,
+    getOrdersByStoreId,
+    updateShippingStatusByStoreAndOrderId,
 }
--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const { createOrder } = require("../controller/orderController");
+const { createOrder, getOrdersByUserId, getOrdersByStoreId, updateShippingStatusByStoreAndOrderId } = require("../controller/orderController");
 const router = express.Router();
 
-
+// create order
 router.post("/", createOrder)
-router.get("/allOrders/:storeId",)
-router.patch("/update/shipping/:orderId",)
+
+// get my order by user id
+router.get("/my-orders/:userId", getOrdersByUserId)
+
+// get all orders by store id
+router.get("/store/:storeId", getOrdersByStoreId)
+
+// update sipping status by store and order id
+router.put("/update/status/:storeId/:orderId", updateShippingStatusByStoreAndOrderId)
 
 
 
