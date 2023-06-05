7e409292abb98c2a93c110f47329599243825dc6
Robiulalam76 <robiulalam762400@gmail.com>
2023-06-05 18:39:39 +0600

    updated

--- routes/storeRoutes.js
+++ routes/storeRoutes.js
@@ -10,12 +10,10 @@ const {
   deleteSingleStore,
   getVerifiedStores,
   getAllStoresByRole,
-  addProperty,
   updateStatus,
   updateStoreByStoreId,
 } = require("../controller/storeController");
 const { isAuth } = require("../config/auth");
-// const { protect } = require("../middleware/authMiddleware");
 
 router.route("/").post(addStore).get(getStore);
 router.route("/getVerifiedStores").get(getVerifiedStores);
@@ -23,14 +21,10 @@ router.route("/:id").get(getStoreById);
 router.get("/getInfo/:username", getStoreByUsername);
 router.post("/add/:id", addStoreBySeller);
 router.delete("/:id", deleteSingleStore);
-// router.route("/myorders").get(protect, getMyOrders);
-// router.route('/:id/pay').put(protect, updateOrderToPaid)
-// router.route('/:id/deliver').put(protect, admin, updateOrderToDelivered);
 
 router.patch("/update/:storeId", updateStoreByStoreId)
 
 router.put("/status/:id", isAuth, updateStatus)
 router.get("/getAllStores/byRole", isAuth, getAllStoresByRole)
-// router.patch("/addProperty", addProperty)
 
 module.exports = router;
